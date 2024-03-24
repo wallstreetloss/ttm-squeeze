77cb2dab20592857fb31ab737d67f39ed501a0a4
Katie Lin <chiachen@ualberta.ca>
2024-03-24 15:31:31 -0700

    added momentum indicator

--- snapshot.py
+++ snapshot.py
@@ -5,5 +5,5 @@ with open('symbols.csv') as f:
     lines = f.read().splitlines()
     for symbol in lines:
         print(symbol)
-        data = yf.download(symbol, start="2020-01-01", end="2020-08-22")
+        data = yf.download(symbol, start="2023-06-01")
         data.to_csv("datasets/{}.csv".format(symbol))
--- squeeze.py
+++ squeeze.py
@@ -1,5 +1,7 @@
 import os, pandas
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import numpy as np
 
 dataframes = {}
 
@@ -22,13 +24,32 @@ for filename in os.listdir('datasets'):
     df['lower_keltner'] = df['20sma'] - (df['ATR'] * 1.5)
     df['upper_keltner'] = df['20sma'] + (df['ATR'] * 1.5)
 
-    def in_squeeze(df):
-        return df['lower_band'] > df['lower_keltner'] and df['upper_band'] < df['upper_keltner']
+    #Momentum Indicator
+    df['donchian_midline'] = (df['High'].rolling(window=20).max()+df['Low'].rolling(window=20).min())/2
+    df['histogram_bar'] = df['Close'] - ((df['donchian_midline']+ df['20sma'])/2)
+    fit_y = np.array(range(0,20))
+    df['histogram_bar'] = df['histogram_bar'].rolling(window = 20).apply(lambda x : np.polyfit(fit_y, x, 1)[0] * (20-1) +np.polyfit(fit_y, x, 1)[1], raw=True)
 
-    df['squeeze_on'] = df.apply(in_squeeze, axis=1)
+    # Squeeze on/off df
+    df['squeeze_on'] = (df['lower_band'] > df['lower_keltner']) & (df['upper_band'] < df['upper_keltner'])
+    df['squeeze_off'] = (df['lower_band'] < df['lower_keltner']) & (df['upper_band'] > df['upper_keltner'])
 
-    if df.iloc[-3]['squeeze_on'] and not df.iloc[-1]['squeeze_on']:
-        print("{} is coming out the squeeze".format(symbol))
+    #TODO implement
+    # # entry point for long position:
+    # # 1. black cross becomes gray (the squeeze is released)
+    # long_cond1 = (df['squeeze_off'][-2] == False) & (df['squeeze_off'][-1] == True) 
+    # # 2. bar value is positive => the bar is light green
+    # long_cond2 = df['value'][-1] > 0
+    # enter_long = long_cond1 and long_cond2
+    # # entry point for short position:
+    # # 1. black cross becomes gray (the squeeze is released)
+    # short_cond1 = (df['squeeze_off'][-2] == False) & (df['squeeze_off'][-1] == True) 
+    # # 2. bar value is negative => the bar is light red 
+    # short_cond2 = df['value'][-1] < 0
+    # enter_short = short_cond1 and short_cond2
+
+    # if df.iloc[-3]['squeeze_on'] and not df.iloc[-1]['squeeze_on']:
+    #     print("{} is coming out the squeeze".format(symbol))
 
     # save all dataframes to a dictionary
     # we can chart individual names below by calling the chart() function
@@ -42,11 +63,23 @@ def chart(df):
 
     upper_keltner = go.Scatter(x=df['Date'], y=df['upper_keltner'], name='Upper Keltner Channel', line={'color': 'blue'})
     lower_keltner = go.Scatter(x=df['Date'], y=df['lower_keltner'], name='Lower Keltner Channel', line={'color': 'blue'})
+    plot1_list = [candlestick, upper_band, lower_band, upper_keltner, lower_keltner]
+    #histogram
+    histogram = go.Bar(x=df['Date'], y=df['histogram_bar'], name='Momentum Histogram', marker_color=['red' if val <=0 else 'green' for val in df['histogram_bar']])
+    squeeze_on_off = go.Scatter(x=df['Date'], y=[0]*len(df), name='Squeeze on/off dots',marker=dict(
+        color=['grey' if val else 'black' for val in df['squeeze_off']],
+        size=5,  # Adjust the marker size as per your requirement
+        ), mode='markers')
 
-    fig = go.Figure(data=[candlestick, upper_band, lower_band, upper_keltner, lower_keltner])
-    fig.layout.xaxis.type = 'category'
-    fig.layout.xaxis.rangeslider.visible = False
-    fig.show()
+    subplots= make_subplots(rows=2, cols=1, shared_xaxes=True)
 
-df = dataframes['GOOGL']
+    for i in plot1_list: 
+        subplots.add_trace(i, row=1, col=1)
+
+    subplots.add_trace(histogram, row=2, col=1)
+    subplots.add_trace(squeeze_on_off, row=2, col=1)
+
+    subplots.show()
+
+df = dataframes['TSLA']
 chart(df)
